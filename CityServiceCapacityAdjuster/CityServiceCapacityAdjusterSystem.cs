689a89857c4a316ab43f79bf9e157186c7d3baa6
Patryk <levengothon@gmail.com>
2024-12-25 00:04:18 +0100

    Add project files.

---
+++ CityServiceCapacityAdjuster/CityServiceCapacityAdjusterSystem.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+using Colossal.IO.AssetDatabase.Internal;
+using Colossal.Serialization.Entities;
+using Game;
+using Game.Companies;
+using Game.Prefabs;
+using Unity.Collections;
+using Unity.Entities;
+
+namespace CityServiceCapacityAdjuster
+{
+    public partial class CityServiceCapacityAdjusterSystem : GameSystemBase
+    {
+        private readonly Dictionary<Entity, CargoTransportStationData> _cargoStationData =
+            new Dictionary<Entity, CargoTransportStationData>();
+
+        private readonly Dictionary<Entity, PostFacilityData> _postSortingData =
+            new Dictionary<Entity, PostFacilityData>();
+
+        private EntityQuery _queryA;
+        private EntityQuery _queryB;
+
+        private void UpdateCargoTransportStationTruckCapacity()
+        {
+            var xA = _queryA.ToEntityArray(Allocator.Temp);
+            var TruckSliderFactor = Mod.m_Setting.CargoTruckSlider;
+
+            var componentData2 = default(TransportCompanyData);
+            componentData2.m_MaxTransports = TruckSliderFactor * 16;
+
+            foreach (var x in xA)
+            {
+                CargoTransportStationData data;
+
+                if (!_cargoStationData.TryGetValue(x, out data))
+                {
+                    data = EntityManager.GetComponentData<CargoTransportStationData>(x);
+                    _cargoStationData.Add(x, data);
+                }
+
+                Mod.log.Info($"Truck multiplier: {TruckSliderFactor}, final capacity: {TruckSliderFactor * 16}");
+
+                EntityManager.SetComponentData(x, componentData2);
+            }
+        }
+
+        private void UpdatePostFacilitySortingRate()
+        {
+            var xB = _queryB.ToEntityArray(Allocator.Temp);
+            var PostSortingSliderFactor = Mod.m_Setting.PostSortingSlider;
+
+            foreach (var x in xB)
+            {
+                PostFacilityData data;
+
+                if (!_postSortingData.TryGetValue(x, out data))
+                {
+                    data = EntityManager.GetComponentData<PostFacilityData>(x);
+                    _postSortingData.Add(x, data);
+                }
+
+                Mod.log.Info($"Factor Post {PostSortingSliderFactor},{PostSortingSliderFactor * data.m_SortingRate}");
+
+                data.m_SortingRate = PostSortingSliderFactor * data.m_SortingRate;
+                EntityManager.SetComponentData(x, data);
+            }
+        }
+
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+
+            _queryA = GetEntityQuery(new EntityQueryDesc
+            {
+                All = new[]
+                {
+                    ComponentType.ReadWrite<CargoTransportStationData>()
+                }
+            });
+          
+            _queryB = GetEntityQuery(new EntityQueryDesc
+            {
+                All = new[]
+                {
+                    ComponentType.ReadWrite<PostFacilityData>()
+                }
+            });
+           
+
+            RequireForUpdate(_queryA);
+            RequireForUpdate(_queryB);
+        }
+
+        protected override void OnGameLoadingComplete(Purpose purpose, GameMode mode)
+        {
+            Mod.log.Info("On game loading");
+            base.OnGameLoadingComplete(purpose, mode);
+
+            UpdateCargoTransportStationTruckCapacity();
+            UpdatePostFacilitySortingRate();
+
+            Enabled = false;
+        }
+
+        protected override void OnUpdate()
+        {
+            Mod.log.Info("On game update");
+            if (Enabled)
+            {
+                UpdateCargoTransportStationTruckCapacity();
+                UpdatePostFacilitySortingRate();
+
+                Enabled = false;
+            }
+        }
+    }
+}
---
+++ CityServiceCapacityAdjuster/Setting.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections.Generic;
+using Colossal;
+using Colossal.IO.AssetDatabase;
+using Game.Modding;
+using Game.Settings;
+using Game.UI;
+using Unity.Entities;
+
+//test
+
+namespace CityServiceCapacityAdjuster
+{
+    [FileLocation(nameof(CityServiceCapacityAdjuster))]
+    [SettingsUIGroupOrder(kSliderGroup)]
+    [SettingsUIShowGroupName(kSliderGroup)]
+    public class Setting : ModSetting
+    {
+        public const string kSection = "Main";
+        public const string kSliderGroup = "Slider";
+
+        public Setting(IMod mod) : base(mod)
+        {
+            if (CargoTruckSlider == 0) SetDefaults();
+        }
+
+        [SettingsUISlider(min = 1, max = 5, step = 1, scalarMultiplier = 1, unit = Unit.kInteger)]
+        [SettingsUISection(kSection, kSliderGroup)]
+        public int CargoTruckSlider { get; set; }
+
+        [SettingsUISlider(min = 1, max = 5, step = 1, scalarMultiplier = 1, unit = Unit.kInteger)]
+        [SettingsUISection(kSection, kSliderGroup)]
+        public int PostSortingSlider { get; set; }
+
+        [SettingsUISection(kSection, kSliderGroup)]
+        [SettingsUIButton]
+        [SettingsUIConfirmation]
+        public bool ResetSettings
+        {
+            set
+            {
+                SetDefaults();
+                ApplyAndSave();
+            }
+        }
+
+        [SettingsUISection(kSection, kSliderGroup)]
+        [SettingsUIButton]
+        [SettingsUIConfirmation]
+        public bool ApplySettings
+        {
+            set
+            {
+                ApplyAndSave();
+            }
+        }
+
+
+        public override void SetDefaults()
+        {
+            CargoTruckSlider = 1;
+            PostSortingSlider = 1;
+        }
+
+        public override void Apply()
+        {
+            base.Apply();
+            var system = World.DefaultGameObjectInjectionWorld
+                .GetExistingSystemManaged<CityServiceCapacityAdjusterSystem>();
+            system.Enabled = true;
+        }
+    }
+
+    public class LocaleEN : IDictionarySource
+    {
+        private readonly Setting m_Setting;
+
+        public LocaleEN(Setting setting)
+        {
+            m_Setting = setting;
+        }
+
+        public IEnumerable<KeyValuePair<string, string>> ReadEntries(IList<IDictionaryEntryError> errors,
+            Dictionary<string, int> indexCounts)
+        {
+            return new Dictionary<string, string>
+            {
+                { m_Setting.GetSettingsLocaleID(), "City Service Capacity Adjuster" },
+                { m_Setting.GetOptionTabLocaleID(Setting.kSection), "Main Settings" },
+
+                { m_Setting.GetOptionGroupLocaleID(Setting.kSliderGroup), "Capacity Sliders" },
+
+                { m_Setting.GetOptionLabelLocaleID(nameof(Setting.CargoTruckSlider)), "Cargo Station Truck Capacity" },
+                {
+                    m_Setting.GetOptionDescLocaleID(nameof(Setting.CargoTruckSlider)),
+                    "Set multiplier for truck capacity in Cargo Station (base 16 * factor)"
+                },
+                { m_Setting.GetOptionLabelLocaleID(nameof(Setting.PostSortingSlider)), "Post Office Sorting Rate" },
+                {
+                    m_Setting.GetOptionDescLocaleID(nameof(Setting.PostSortingSlider)),
+                    "Set Multiplier for sorting speed in Sorting Facility (base 50k * factor)"
+                },
+
+                { m_Setting.GetOptionLabelLocaleID(nameof(Setting.ResetSettings)), "Reset to defaults" },
+                { m_Setting.GetOptionLabelLocaleID(nameof(Setting.ApplySettings)), "Apply changes" },
+
+                { m_Setting.GetOptionWarningLocaleID(nameof(Setting.ResetSettings)), "Accept Reset" },
+                { m_Setting.GetOptionWarningLocaleID(nameof(Setting.ApplySettings)), "Accept new values" }
+            };
+        }
+
+        public void Unload()
+        {
+        }
+    }
+}
